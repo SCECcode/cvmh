6122cc96f10a02d4518286d3b37ccf4bf52c6b36
Mei-Hui Su <mei@discovery1.usc.edu>
2021-10-14 14:06:32 -0700

    rework the data directory

--- model/make_data_files.py
+++ model/make_data_files.py
@@ -11,43 +11,86 @@ import subprocess
 
 model = "CVMH"
 
+if sys.version_info.major >= (3) :
+  from urllib.request import urlopen
+else:
+  from urllib2 import urlopen
+
 def usage():
-    print("\n./make_data_files.py -d [cvmh1511] -u [uid]\n\n")
-    print("-d - dataset to retrieve from hypocenter.\n")
-    print("-u - username to use to do the dataset retrieval.\n")
+    print("\n./make_data_files.py\n\n")
     sys.exit(0)
+
+def download_urlfile(url,fname):
+  try:
+    response = urlopen(url)
+    CHUNK = 16 * 1024
+    with open(fname, 'wb') as f:
+      while True:
+        chunk = response.read(CHUNK)
+        if not chunk:
+          break
+        f.write(chunk)
+  except:
+    e = sys.exc_info()[0]
+    print("Exception retrieving and saving model datafiles:",e)
+    raise
+  return True
 
 def main():
 
     # Set our variable defaults.
-    dataset = -1
-    username = -1 
-    path = "/var/www/html/research/ucvmc/" + model 
+    path = ""
 
-    # Get the dataset.
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "u:d:", ["user=", "dataset="])
-    except getopt.GetoptError as err:
-        print(str(err))
-        usage()
+        fp = open('./config','r')
+    except:
+        print("ERROR: failed to open config file")
         sys.exit(1)
 
-    for o, a in opts:
-        if o in ("-d", "--dataset"):
-            dataset = str(a)
-        if o in ("-u", "--user"):
-            username = str(a) + "@"
+    ## look for model_data_path and other varaibles
+    lines = fp.readlines()
+    for line in lines :
+        if line[0] == '#' :
+          continue
+        parts = line.split('=')
+        if len(parts) < 2 :
+          continue;
+        variable=parts[0].strip()
+        val=parts[1].strip()
 
-    # If the iteration number was not provided, display the usage.
-    if dataset == -1 or username == -1:
-        usage()
+        if (variable == 'model_data_path') :
+            path = val + '/' + model
+            continue
+        if (variable == 'model_dir') :
+            mdir = "./"+val
+            continue
+        continue
+    if path == "" :
+        print("ERROR: failed to find variables from config file")
         sys.exit(1)
+
+    fp.close()
 
     print("\nDownloading model dataset\n")
 
-    subprocess.check_call(["scp", username +
-                           "hypocenter.usc.edu:" + path + "/" + dataset +"/*",
-                           "."])
+    subprocess.check_call(["mkdir", "-p", "./"+mdir])
+
+    flist=['base@@', 'BASE.gts', 'BATO.gts', 'CVM_CM_TAG@@', 'CVM_CM.vo', 'CVM_CM_VP@@', 'CVM_CM_VS@@', 'CVM_HR_TAG@@', 'CVM_HR.vo', 'CVM_HR_VP@@', 'CVM_HR_VS@@', 'CVM_LR.vo', 'CVMSM_flags@@', 'CVMSM_tag66@@', 'CVMSM_vp66@@', 'CVMSM_vs66@@', 'cvm_vs30_wills.hdr', 'cvm_vs30_wills.mdl', 'interfaces.vo', 'model_top@@', 'moho@@', 'MOHO.gts', 'topo_dem@@']
+
+    for f in flist :
+        fname = mdir + "/" +f
+        url = path + "/" + fname
+        print(url, fname)
+        download_urlfile(url,fname)
+
+    subprocess.check_call(["mkdir", "-p", "./"+mdir+'/tsurf'])
+
+    fflist=['tsurf/CMxVM_Model3D_CalMex_BATO.ts', 'tsurf/CMxVM_Model3D_CM_BASE_Folded.dxf', 'tsurf/CMxVM_Model3D_CM_BASE_Folded.ts', 'tsurf/CVMH_Basement64.ts', 'tsurf/CVMH_CalMex_BATO.ts', 'tsurf/CVMH_Moho64.ts', 'tsurf/CVMH_Moho.ts']
+    for ff in fflist :
+        fname = mdir + "/" +ff
+        url = path + "/" + fname
+        print(url, fname)
+        download_urlfile(url,fname)
 
     print("\nDone!")
 
