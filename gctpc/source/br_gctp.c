0eb8fee3948f8b57cfdaffd105168ff5e49bf4f1
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2026-08-14 04:43:20 +0000

    update br_gctp.c from Barall, Michael <mbarall@usgs.gov>

--- gctpc/source/br_gctp.c
+++ gctpc/source/br_gctp.c
@@ -2,7 +2,29 @@
 #ifdef unix
 /*  Fortran bridge routine for the UNIX */
 
-gctp_(incoor,insys,inzone,inparm,inunit,indatum,ipr,efile,jpr,pfile,
+void gctp(
+double *incoor,		/* input coordinates				*/
+long *insys,		/* input projection code			*/
+long *inzone,		/* input zone number				*/
+double *inparm,		/* input projection parameter array		*/
+long *inunit,		/* input units					*/
+long *indatum,		/* input datum 					*/
+long *ipr,		/* printout flag for error messages. 0=screen, 1=file, 2=both*/
+char *efile,		/* error file name				*/
+long *jpr,		/* printout flag for projection parameters 0=screen, 1=file, 2 = both*/
+char *pfile,		/* error file name				*/
+double *outcoor,	/* output coordinates				*/
+long *outsys,		/* output projection code			*/
+long *outzone,		/* output zone					*/
+double *outparm,	/* output projection array			*/
+long *outunit,		/* output units					*/
+long *outdatum,		/* output datum					*/
+char fn27[],		/* file name of NAD 1927 parameter file		*/
+char fn83[], 	 	/* file name of NAD 1983 parameter file		*/
+long *iflg		/* error flag					*/
+);
+
+void gctp_(incoor,insys,inzone,inparm,inunit,indatum,ipr,efile,jpr,pfile,
                outcoor, outsys,outzone,outparm,outunit,fn27,fn83,iflg)
 
 double *incoor;
@@ -20,11 +42,15 @@ long *outsys;
 long *outzone;
 double *outparm;
 long *outunit;
+char *fn27;
+char *fn83;
 long *iflg;
 
 {
+long outdatum = 0;
+
 gctp(incoor,insys,inzone,inparm,inunit,indatum,ipr,efile,jpr,pfile,outcoor,
-     outsys,outzone,outparm,outunit,fn27,fn83,iflg);
+     outsys,outzone,outparm,outunit,&outdatum,fn27,fn83,iflg);
 
 return;
 }
